7cb8ba57d382fb3f115efe7731c98553b5b73268
miriamfrank23 <miriamfrank23@gmail.com>
2018-08-10 15:03:37 -0400

    prime

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(int)
+if int <= 1
+  false
+elsif int == 2 || int == 3
+  true
+elsif int > 3
+  array_of_nums = (2..(int - 1)).to_a
+  array_of_nums.each do |num|
+  return false if int % num == 0
+   end
+   true
+  end
+end
